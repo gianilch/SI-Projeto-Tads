08b37f7f4b7bde8c37e55719993d61ea94ba7f26
gianilch <giancilchechen@gmail.com>
2024-11-20 22:15:34 -0300

    refactor: removido switchcase desnecessário

--- controller/pagamento.php
+++ controller/pagamento.php
@@ -1,13 +1,5 @@
 <?php
 
-    include("../model/banco.php");
-    include("../view/template.php");
-
-    switch(@$_REQUEST["page"]){
-        default:
-            include("../view/pagamento/pagamento.php");
-            break;
-
-    }
-
-?>
+include("../model/banco.php");
+include("../view/template.php");
+include("../view/pagamento/pagamento.php");
