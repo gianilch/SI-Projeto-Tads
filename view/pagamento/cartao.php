d2558d1ff8be2e8799268e0086cab100b814fa88
gianilch <giancilchechen@gmail.com>
2024-11-21 16:31:35 -0300

    feat: base para cartão

--- view/pagamento/cartao.php
+++ view/pagamento/cartao.php
@@ -1,1 +1,17 @@
-<h1>CARTAO</h1>
+<div class="container mt-5">
+    <form action="?page=salvar" method="POST">
+        <div class="form-group">
+            <label for="nome">Nome *</label>
+            <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome" required>
+        </div>
+        <div class="form-group">
+            <label for="cpf">CPF *</label>
+            <input type="text" maxlength="11" class="form-control" id="cpf" name="cpf" placeholder="Digite seu CPF" required>
+        </div>
+        <div class="form-group">
+            <label for="numeroCartao">Numero do Cartão *</label>
+            <input type="text" maxlength="16" minlegth="13" class="form-control" id="numeroCartao" name="numeroCartao" placeholder="Digite o Numero do Cartão" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Concluir Pagamento</button>
+    </form>
+</div>
