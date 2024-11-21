9710a2b1174f7e70ddd8b1965d22d5f088ea2f96
gianilch <giancilchechen@gmail.com>
2024-11-20 22:16:12 -0300

    feat: Incluido logica de verificar forma de pagamento

--- view/pagamento/pagamento.php
+++ view/pagamento/pagamento.php
@@ -1,13 +1,26 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $paymentType = isset($_POST['paymentTypeRadioOptions']) ? $_POST['paymentTypeRadioOptions'] : '';
+}
+?>
 <div class="container mt-5">
     <h2>Formas de Pagamento</h2>
-    <form action="?page=salvar" method="POST">
+    <form action="" method="POST">
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="paymentTypeRadioOptions" id="radioPix" value="pix">
-            <label class="form-check-label" for="inlineRadio1">PIX</label>
+            <input class="form-check-input" type="radio" name="paymentTypeRadioOptions" id="radioPix" value="pix" <?php if (isset($paymentType) && $paymentType == 'pix') echo 'checked'; ?> onchange="this.form.submit()">
+            <label class="form-check-label" for="radioPix">PIX</label>
         </div>
         <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="paymentTypeRadioOptions" id="radioCartao" value="cartao">
-            <label class="form-check-label" for="inlineRadio2">Cartao</label>
+            <input class="form-check-input" type="radio" name="paymentTypeRadioOptions" id="radioCartao" value="cartao" <?php if (isset($paymentType) && $paymentType == 'cartao') echo 'checked'; ?> onchange="this.form.submit()">
+            <label class="form-check-label" for="radioCartao">Cartão</label>
         </div>
     </form>
+
+    <?php
+    if (isset($paymentType) && $paymentType == 'pix') {
+        include 'pix.php';
+    } elseif (isset($paymentType) && $paymentType == 'cartao') {
+        include 'cartao.php';
+    }
+    ?>
 </div>
